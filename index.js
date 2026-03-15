09659164b324e4649bb1996be36d800ae09cfbcf
Idan Ekitela <idanwalker87@gmail.com>
2026-03-15 13:12:57 +0300

    Set up Express server with a basic route

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Bot is running 🚀")
+})
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
 const { 
   default: makeWASocket,
   useMultiFileAuthState,
